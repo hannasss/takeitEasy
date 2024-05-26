73a779e99c7d03a61dc661f664fcbfba8aaa91dc
cocoflsl <102604926+cocoflsl@users.noreply.github.com>
2024-05-27 02:49:00 +0900

    Update MainActivity.java

--- app/src/main/java/com/play/ME/MainActivity.java
+++ app/src/main/java/com/play/ME/MainActivity.java
@@ -68,12 +68,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
     
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            installSplashScreen()
-            setContentView(R.layout.activity_main)
-        }
-    }
-    
 }
